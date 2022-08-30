c18c99b150fcfe82ce9c6a1846635616137ed646
4rlm <4rlm@protonmail.ch>
2022-08-30 15:37:47 +0900

    js form validation

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+const form = document.getElementById('form');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
 
+let isValid = false;
 
+function validateForm() {
+  // Using Constraint API
+  isValid = form.checkValidity();
+  // Style main message for an error
+  message.textContent = 'Please fill out all fields.';
+  message.style.color = 'red';
+  messageContainer.style.borderColor = 'red';
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  validateForm();
+}
+
+// Event Listener
+form.addEventListener('submit', processFormData);
